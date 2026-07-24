90ddeff9a78c283911a340795174eca38a079399
RJ <r.jennings@micrantha.com>
2026-07-24 00:34:51 -0700

    ci: stabilize components packaging and release checks (#48)
    Add explicit components package CI, shared package validation, release safeguards, dedicated Jest configuration, and hardened package test coverage.

--- packages/amaryllis-components/jest.config.js
+++ packages/amaryllis-components/jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      { tsconfig: '<rootDir>/tsconfig.test.json' },
+    ],
+  },
 };
