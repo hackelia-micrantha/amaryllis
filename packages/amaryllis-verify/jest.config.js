bce4522dd1b412615e18973444c4de964b79d89c
RJ <rybertjen@gmail.com>
2026-08-13 01:26:43 -0700

    verify: configure validation jest

---
+++ packages/amaryllis-verify/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.test.json' }],
+  },
+};
