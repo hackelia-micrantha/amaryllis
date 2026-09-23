60a9bf132a5c9283a4e9f7333c652e7d51084b3a
RJ <r.jennings@micrantha.com>
2026-09-23 01:18:26 -0700

    runtime: remove RxJS async bridge (#141)
    Remove the observable bridge and internalize async-result accumulation with terminal lifecycle ownership. Isolate consumer callback exceptions for custom engines and cover synchronous emission with regression tests.
    
    Closes #105

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 export { default as Amaryllis, LlmPipe } from './Amaryllis';
 export * from './AmaryllisHooks';
 export * from './AmaryllisContext';
-export * from './AmaryllisRx';
 export * from './Errors';
 export { gemmaProtocol } from './GemmaProtocol';
 export { createContextEngine } from '@micrantha/amaryllis/context';
